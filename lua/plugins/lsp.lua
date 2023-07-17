b35760ffce764236ac56dd21e3507a5f74423631
tncrdk <thnchrdjk@gmail.com>
2023-07-17 18:31:22 +0200

    påbegynt clangd oppsett

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -128,34 +128,11 @@ return {
 				},
 			})
 
-			-- require("lspconfig").pylsp.setup({
+			-- require'lspconfig'.clangd.setup{
 			-- 	on_attach = on_attach,
 			-- 	capabilities = capabilities,
-			-- 	settings = {
-			-- 		pylsp = {
-			-- 			plugins = {
-			-- 				flake8 = {
-			-- 					enabled = true,
-			-- 					maxLineLength = 88, -- Black's line length
-			-- 				},
-			-- 				-- Disable plugins overlapping with flake8
-			-- 				pycodestyle = {
-			-- 					enabled = false,
-			-- 				},
-			-- 				mccabe = {
-			-- 					enabled = false,
-			-- 				},
-			-- 				pyflakes = {
-			-- 					enabled = false,
-			-- 				},
-			-- 				-- Use Black as the formatter
-			-- 				autopep8 = {
-			-- 					enabled = false,
-			-- 				},
-			-- 			},
-			-- 		},
-			-- 	},
-			-- })
+			-- }
+
 		end,
 	},
 }
