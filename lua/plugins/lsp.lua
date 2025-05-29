309ebd95ffe518867f36e88708977f6724884bcf
Thorbjørn Djuvpik <thorbjorn.djupvik@gmail.com>
2025-05-29 21:45:11 +0200

    removed R language server

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -139,9 +139,6 @@ return {
                 },
             })
 
-            -- R
-            require("lspconfig").r_language_server.setup({})
-
             -- require'lspconfig'.clangd.setup{
             -- 	on_attach = on_attach,
             -- 	capabilities = capabilities,
