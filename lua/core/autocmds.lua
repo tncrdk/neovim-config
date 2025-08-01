cfb5fd274ba04c32f513dd9c3919c0bbde6c0a04
Thorbjørn Djupvik <thorbjorn.djupvik@gmail.com>
2025-08-01 09:53:35 +0200

    updated plugin desc

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -2,40 +2,41 @@
 vim.api.nvim_create_autocmd('LspAttach', {
   -- Create a group for lsp-attach autocmds
   group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+  -- Callback
   callback = function(ev)
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
 
     -- Function for mappping keys
-    local map = function(keys, func, desc)
+    local lsp_map = function(keys, func, desc)
       vim.keymap.set("n", keys, func, { buffer = ev.buf, desc = "LSP: " .. desc })
     end
 
     -- Key-mappings
-    map("<leader>lr", vim.lsp.buf.rename, "Rename symbol")
-    map("<leader>la", vim.lsp.buf.code_action, "Code action")
-    map("<leader>ld", vim.lsp.buf.type_definition, "Type definition")
-    map("<leader>ss", require("telescope.builtin").lsp_document_symbols, "Document symbols")
+    lsp_map("<leader>lr", vim.lsp.buf.rename, "Rename symbol")
+    lsp_map("<leader>la", vim.lsp.buf.code_action, "Code action")
+    lsp_map("<leader>ld", vim.lsp.buf.type_definition, "Type definition")
+    lsp_map("<leader>ss", require("telescope.builtin").lsp_document_symbols, "Document symbols")
 
-    map("gd", vim.lsp.buf.definition, "Goto Definition")
-    map("gD", vim.lsp.buf.declaration, "Goto Declaration")
-    map("gr", require("telescope.builtin").lsp_references, "Goto References")
-    map("gI", vim.lsp.buf.implementation, "Goto Implementation")
-    map("gs", vim.lsp.buf.signature_help, "Signature Documentation") -- Ex: Checking arguments of function when supplying them
+    lsp_map("gd", vim.lsp.buf.definition, "Goto Definition")
+    lsp_map("gD", vim.lsp.buf.declaration, "Goto Declaration")
+    lsp_map("gr", require("telescope.builtin").lsp_references, "Goto References")
+    lsp_map("gI", vim.lsp.buf.implementation, "Goto Implementation")
+    lsp_map("gs", vim.lsp.buf.signature_help, "Signature Documentation") -- Ex: Checking arguments of function when supplying them
     -- Diagnostic keymaps
-    map("gx", vim.diagnostic.open_float, "Show diagnostics under cursor")
-    map("K", vim.lsp.buf.hover, "Hover Documentation")
+    lsp_map("gx", vim.diagnostic.open_float, "Show diagnostics under cursor")
+    lsp_map("K", vim.lsp.buf.hover, "Hover Documentation")
     -- Trouble
     -- TODO: Check if needed
-    map("<leader>ls", "<cmd>Trouble symbols toggle focus=false<cr>", "Symbols (Trouble)")
-    map("<leader>ll", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-      "LSP Definitions / references / ... (Trouble)")
+    lsp_map("<leader>ls", "<cmd>Trouble symbols toggle focus=false<cr>", "Symbols (Trouble)")
+    lsp_map("<leader>ll", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      "LSP References (Trouble)")
 
     -- nvim-navbuddy
-    map("<leader>ln", "<cmd>Navbuddy<cr>", "NavBuddy")
+    lsp_map("<leader>ln", "<cmd>Navbuddy<cr>", "NavBuddy")
 
     -- Inlay hints
     if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-      map('<leader>lh', function()
+      lsp_map('<leader>lh', function()
         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
       end, 'Toggle Inlay Hints')
     end
