3a9160b95b1b413e5f7b62feb4a02f7c7cfdae6b
Thorbjørn Djuvpik <thorbjorn.djupvik@gmail.com>
2025-08-16 15:46:13 +0200

    removed super-tab for snippets. Use its own keybinding instead

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -26,7 +26,7 @@ return {
                         luasnip.lsp_expand(args.body)
                     end,
                 },
-                mapping = cmp.mapping.preset.insert({
+                mapping = {
                     -- ["<C-k>"] = cmp.mapping.select_prev_item(),
                     -- ["<C-j>"] = cmp.mapping.select_next_item(),
                     ["<C-k>"] = cmp.mapping.scroll_docs(4),
@@ -40,8 +40,6 @@ return {
                     ["<Tab>"] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.select_next_item()
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
                         else
                             fallback()
                         end
@@ -49,13 +47,11 @@ return {
                     ["<S-Tab>"] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.select_prev_item()
-                        elseif luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
                         else
                             fallback()
                         end
                     end, { "i", "s" }),
-                }),
+                },
                 formatting = {
                     fields = { "kind", "abbr", "menu" },
                     format = function(entry, vim_item)
