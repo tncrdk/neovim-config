da90fd1d6f60fdad7722b7aab938e20b839edaea
Thorbjørn Djuvpik <thorbjorn.djupvik@gmail.com>
2026-09-08 15:11:05 +0200

    added lean

---
+++ lua/plugins/lean.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        'Julian/lean.nvim',
+        -- event = { 'BufReadPre *.lean', 'BufNewFile *.lean' },
+
+        dependencies = {
+            -- optional dependencies:
+
+            -- 'nvim-telescope/telescope.nvim', -- for Lean-specific pickers
+            -- 'andymass/vim-matchup',          -- for enhanced % motion behavior
+            -- 'andrewradev/switch.vim',        -- for switch support
+            -- 'tomtom/tcomment_vim',           -- for commenting
+        },
+
+        ---@type lean.Config
+        init = function()
+            vim.g.lean_config = {
+                mappings = true,
+            }
+        end,
+    }
+}
+-- return {}
